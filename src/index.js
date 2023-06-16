9b1288cfc37d3f327c305a273a1f9b893702337e
PavaniArcot <128066621+PavaniArcot@users.noreply.github.com>
2023-06-16 17:11:18 +0530

    commit
    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import FormValidation from "./formvalidation.js";
+// import Weather from "./weather.js";
+ import ReactDOM from "react-dom"
+ReactDOM.render(
+   <div>
+      <FormValidation/> 
+      
+      
+   </div>
+   
+  
+,document.getElementById("root")) 
